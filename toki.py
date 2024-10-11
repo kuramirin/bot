62c569a295f45093aeac0bcdd4454b65e8d7368b
Amir <amir.kurmanov.08@gmail.com>
2024-10-11 16:47:05 +0300

    Add files via upload

---
+++ toki.py
@@ -0,0 +1,59 @@
+##from configparser import ConfigParser
+
+#config = ConfigParser()
+#config.read("config.ini")
+
+
+BOT_TOKEN ='111111111111111111111111111111111111111111111111'#config.get(section="bot", option= "token")
+ #os.getenv('BOT_TOKEN')
+#if not BOT_TOKEN
+   # exit("Please, provide BOT_TOKEN env variable")
+FROGS_PIC = 'https://i.pinimg.com/originals/98/25/c9/9825c95e8df7826e413a816ce49ce25e.jpg'
+dont_forward_commands = 'Какой остроумный ответ!'
+BOT_ADMIN_USER_IDS = ['775292229',
+                      '124',
+                      '2636']
+secret_message_for_admin = 'password is password'
+secret_message_not_for_admin = 'this is only for admin!'
+md = """*bold \*text*
+_italic \_text_
+__underline__
+~strikethrough~
+||spoiler||
+*bold _italic bold ~italic bold strikethrough ||italic bold strikethrough spoiler||~ __underline italic bold___ bold*
+[inline URL](http://www.example.com/)
+[Amir](tg://user?id=775292229)
+![👍](tg://emoji?id=5368324170671202286)
+`inline fixed-width code`
+```
+pre-formatted fixed-width code block
+```
+```python
+pre-formatted fixed-width code block written in the Python programming language
+```
+>Block quotation started
+>Block quotation continued
+>Block quotation continued
+>Block quotation continued
+>The last line of the block quotation
+**>The expandable block quotation started right after the previous block quotation
+>It is separated from the previous block quotation by an empty bold entity
+>Expandable block quotation continued
+>Hidden by default part of the expandable block quotation started
+>Expandable block quotation continued
+>The last line of the expandable block quotation with the expandability mark||"""
+
+html_text = """<b>bold</b>, <strong>bold</strong>
+<i>italic</i>, <em>italic</em>
+<u>underline</u>, <ins>underline</ins>
+<s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>
+<span class="tg-spoiler">spoiler</span>, <tg-spoiler>spoiler</tg-spoiler>
+<b>bold <i>italic bold <s>italic bold strikethrough <span class="tg-spoiler">italic bold strikethrough spoiler</span></s> <u>underline italic bold</u></i> bold</b>
+<a href="http://www.example.com/">inline URL</a>
+<a href="tg://user?id=75292229">inline mention of a user</a>
+<tg-emoji emoji-id="5368324170671202286">👍</tg-emoji>
+<code>inline fixed-width code</code>
+<pre>pre-formatted fixed-width code block</pre>
+<pre><code class="language-python">pre-formatted fixed-width code block written in the Python programming language</code></pre>
+<blockquote>Block quotation started\nBlock quotation continued\nThe last line of the block quotation</blockquote>
+<blockquote expandable>Expandable block quotation started\nExpandable block quotation continued\nExpandable block quotation continued\nHidden by default part of the block quotation started\nExpandable block quotation continued\nThe last line of the block quotation</blockquote>"""
