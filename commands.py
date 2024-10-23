ecae3f15678454326a3c3da834e1d4b780aea410
Amir <amir.kurmanov.08@gmail.com>
2024-10-23 20:27:05 +0300

    Add files via upload

--- commands.py
+++ commands.py
@@ -3,12 +3,15 @@ from telebot.types import BotCommand
 default_commands = [
     BotCommand("start","приветствие"),
      BotCommand("help", "список команд"),
+     BotCommand("survey", "минизнакомство"),
+     BotCommand("usd_to_rub", "конвертация из доллара в рубль"),
      BotCommand("wiseness","случайная цитата"),
      BotCommand("random_advice", "совет от бота",),
      BotCommand("convert","конвертировать любую валюту в рубли"),
      BotCommand("set_my_currency", "установить целевую валюту", ),
+     BotCommand("art", "моя случайная картинка"),
+     BotCommand("media", "my social media"),
      BotCommand("secret", "only for admin",),
-     BotCommand("set_local_currency", "установить локальную валюту",)
     ]
 
 
